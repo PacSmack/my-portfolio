ae128c1051584209d2f027b8103637b6e04bf126
Nathan da Silva <pacsmacck@gmail.com>
2021-08-18 03:27:25 -0700

    portfolio done

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,69 +6,70 @@ import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
-import itemData from './itemData';
+
+// screenshots import
+import uSing from "../../assets/images/portfolio/0.png"
+import artinerary from "../../assets/images/portfolio/1.png"
+import theTechBlog from "../../assets/images/portfolio/2.png"
+import budgetTracker from "../../assets/images/portfolio/3.png"
+import brazilianQuiz from "../../assets/images/portfolio/4.png"
+import noteTaker from "../../assets/images/portfolio/5.png"
 
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
         flexWrap: 'wrap',
+        position: 'relative',
         justifyContent: 'space-around',
-        overflow: 'hidden',
+        // overflow: 'hidden',
         backgroundColor: theme.palette.background.paper,
     },
     imageList: {
-        width: 500,
-        height: 450,
+        width: "auto",
+        height: "auto",
+        overflow: "hidden"
     },
     icon: {
         color: 'rgba(255, 255, 255, 0.54)',
+        fontWeight: "900"
     },
+    textFormat: {
+        fontWeight: 900
+    }
+
 }));
 
-// screenshots import
-import uSing from "../../assets/images/0.png"
-import artinerary from "../../assets/images/1.png"
-import theTechBlog from "../../assets/images/2.png"
-import budgetTracker from "../../assets/images/3.png"
-import brazilianQuiz from "../../assets/images/4.png"
-import noteTaker from "../../assets/images/5.png"
 
-
-//  * The example data is structured as follows:
-//  *
-//  * import image from 'path/to/image.jpg';
-//  * [etc...]
-//  *
 const itemData = [
     {
-        img: image,
-        title: 'Image',
-        author: 'author',
+        img: uSing,
+        title: 'uSing',
+        githubUrl: 'https://github.com/PacSmack/uSing',
     },
     {
-        img: image,
-        title: 'Image',
-        author: 'author',
+        img: artinerary,
+        title: 'Artinerary',
+        githubUrl: 'https://github.com/PacSmack/ArtInerary',
     },
     {
-        img: image,
-        title: 'Image',
-        author: 'author',
+        img: theTechBlog,
+        title: 'The Tech Blog',
+        githubUrl: 'https://github.com/PacSmack/the-tech-blog',
     },
     {
-        img: image,
-        title: 'Image',
-        author: 'author',
+        img: budgetTracker,
+        title: 'Budget Tracker',
+        githubUrl: 'https://github.com/PacSmack/budget-tracker',
     },
     {
-        img: image,
-        title: 'Image',
-        author: 'author',
+        img: brazilianQuiz,
+        title: 'Brazilian Quiz',
+        githubUrl: 'https://github.com/PacSmack/brazil-quiz',
     },
     {
-        img: image,
-        title: 'Image',
-        author: 'author',
+        img: noteTaker,
+        title: 'Note Taker',
+        githubUrl: 'https://github.com/PacSmack/note-taker',
     }
 ];
 
@@ -78,18 +79,19 @@ export default function Portfolio() {
     return (
         <div className={classes.root}>
             <ImageList rowHeight={180} className={classes.imageList}>
-                <ImageListItem key="Subheader" cols={2} style={{ height: 'auto' }}>
-                    <ListSubheader component="div">December</ListSubheader>
+                <ImageListItem key="Subheader" cols={12} style={{ height: 'auto' }}>
+
                 </ImageListItem>
                 {itemData.map((item) => (
                     <ImageListItem key={item.img}>
                         <img src={item.img} alt={item.title} />
                         <ImageListItemBar
+                            className={classes.textFormat}
                             title={item.title}
-                            subtitle={<span>by: {item.author}</span>}
+                            subtitle={<span className={classes.subtitle}>Repo: <a id="disabled-link" href={item.githubUrl}>{item.githubUrl}</a> </span>}
                             actionIcon={
-                                <IconButton aria-label={`info about ${item.title}`} className={classes.icon}>
-                                    <InfoIcon />
+                                <IconButton
+                                    aria-label={`info about ${item.title}`} className={classes.icon}>
                                 </IconButton>
                             }
                         />
